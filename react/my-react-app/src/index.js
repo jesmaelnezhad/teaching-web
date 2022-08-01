a4e1fb95d496aa2edbee3d23f6f8b6dd9eaa2c62
Jamshid Esmaelnezhad <j.esmaelnezhad@gmail.com>
2022-08-01 13:12:37 +0430

    Adding lifecycle code

--- react/my-react-app/src/index.js
+++ react/my-react-app/src/index.js
@@ -8,11 +8,50 @@ const myFirstElement = <h1>Hello React!</h1>
 const mySecondElement = React.createElement('h1', {}, 'I do not use JSX!');
 
 class Header extends React.Component {
+
+  // mounting 1
   constructor(props) {
     super(props);
+    alert("constructor");
     this.state = {favoritecolor: "red"};
   }
+
+  // mounting 2
+  // updating 1
+  static getDerivedStateFromProps(props, state) {
+    alert("getDerivedStateFromProps");
+    return {favoritecolor: state.favoritecolor}
+  }
+
+  // updating 2
+  shouldComponentUpdate() {
+    alert("shouldComponentUpdate");
+    return true;
+  }
+
+  // mounting 3
+  // updating 3
+  render() {
+    alert("render")
+    return (
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+    );
+  }
+
+  // updating 4
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    alert("getSnapshotBeforeUpdate");
+    return null
+  }
+
+  // updating 5
+  componentDidUpdate() {
+    alert("componentDidUpdate");
+  }
+
+  // mounting 4
   componentDidMount() {
+    alert("componentDidMount");
     setTimeout(() => {
       // you will see that componentDidMount is executed only once
       let idx = Math.floor(Math.random() * 3);
@@ -20,12 +59,13 @@ class Header extends React.Component {
       this.setState({favoritecolor: colors[idx]})
     }, 1000)
   }
-  render() {
-    return (
-      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-    );
+
+  // unmounting 1
+  componentWillUnmount() {
+    alert("componentWillUnmount");
   }
+
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Header />);
+root.render(<Header favcol="green"/>);
